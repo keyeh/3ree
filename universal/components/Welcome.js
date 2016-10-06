ac85ea254dea33be146d458119ae68c7eeafb239
keyeh <keyeh@users.noreply.github.com>
2016-10-06 08:34:51 +0000

    homepage

--- universal/components/Welcome.js
+++ universal/components/Welcome.js
@@ -3,10 +3,17 @@ import React, { PropTypes, Component } from 'react';
 export default class Welcome extends Component {
   render() {
     return (
-      <div>
-        Changes:
+      <div style={{marginLeft: 150}}>
+        <h2>Major Changes</h2>
         <br/>
-        You need to be logged in to create events.
+        <p>
+        	HMR for reducers is broken<br/>
+        	HMR is broken for everything (I think, at least on my machine)<br/>
+        	You need to be logged in to edit events.<br/>
+        	userID is set using the name you log in with<br/>
+        	Render children instead of specific components in PulseApp.js<br/>
+        	Refactor rootReducer/combinereducer<br/>
+        </p>
       </div>
     );
   }
